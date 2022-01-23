f6f861c8cac220470e2e8c5a9f331b5bbcdcc717
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-23 15:15:22 +0100

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Srinivasan Rajendran on 2022-01-23.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
