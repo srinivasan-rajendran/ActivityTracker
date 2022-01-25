be13dbc171345835b9ab71dd4cf444d6ca961c3e
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-25 20:16:01 +0100

    add home view and projects view. populate preview items and environment objects for each preview. add fetcvh request for projects based on the is closed or open property on projects

---
+++ ActivityTracker/ProjectsView.swift
@@ -0,0 +1,54 @@
+//
+//  ProjectsView.swift
+//  ActivityTracker
+//
+//  Created by Srinivasan Rajendran on 2022-01-25.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        projects = FetchRequest<Project>(entity: Project.entity(),
+                                         sortDescriptors: [
+                                            NSSortDescriptor(keyPath: \Project.creationDate,
+                                                             ascending: false)
+                                         ],
+                                         predicate: NSPredicate(format: "closed = %d", showClosedProjects)
+                                        )
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue) { project in
+                    Section {
+                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
+                            Text(item.title ?? "")
+                        }
+                    } header: {
+                        Text(project.title ?? "")
+                    }
+
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(showClosedProjects ? "ClosedProjects" : "Open Projects")
+        }
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+
+    static var dataController = DataController.preview
+
+    static var previews: some View {
+        ProjectsView(showClosedProjects: false)
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
