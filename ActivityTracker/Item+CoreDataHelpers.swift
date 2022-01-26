6100e3a4d063496ac39d1d478fd4dd3bd6469686
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-26 11:52:59 +0100

    add core data helpers in entity extensions to help simplify the code and readable and less optional chaining

---
+++ ActivityTracker/Item+CoreDataHelpers.swift
@@ -0,0 +1,35 @@
+//
+//  Item+CoreDataHelpers.swift
+//  ActivityTracker
+//
+//  Created by Srinivasan Rajendran on 2022-01-26.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+
+    var itemDetail: String {
+        detail ?? ""
+    }
+
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+
+    static var example: Item {
+        let controller = DataController.init(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "THis is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+
+        return item
+    }
+}
--- ActivityTracker/ProjectsView.swift
+++ ActivityTracker/ProjectsView.swift
@@ -27,11 +27,11 @@ struct ProjectsView: View {
             List {
                 ForEach(projects.wrappedValue) { project in
                     Section {
-                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
-                            Text(item.title ?? "")
+                        ForEach(project.projectItems) { item in
+                            Text(item.itemTitle)
                         }
                     } header: {
-                        Text(project.title ?? "")
+                        Text(project.projectTitle)
                     }
 
                 }
