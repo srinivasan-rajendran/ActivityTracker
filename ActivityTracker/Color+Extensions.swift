5f8a6d4f7fc126fad6ca8dacfc885e5de1f7dffd
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-02-06 21:30:09 +0100

    add view for home screen by showing the top priority projects and items in a list

---
+++ ActivityTracker/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  ActivityTracker
+//
+//  Created by Srinivasan Rajendran on 2022-02-06.
+//
+
+import Swift
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
