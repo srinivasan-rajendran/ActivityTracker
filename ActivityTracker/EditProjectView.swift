156aebe39d65a0a4be11037f1b4cfc71803f0b8b
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-02-09 20:21:05 +0100

    add accessibility support to the application.

--- ActivityTracker/EditProjectView.swift
+++ ActivityTracker/EditProjectView.swift
@@ -57,6 +57,11 @@ struct EditProjectView: View {
                             color = item
                             update()
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityAddTraits(
+                            item == color ? [.isButton, .isSelected] : .isButton
+                        )
+                        .accessibilityLabel(LocalizedStringKey(item))
                     }
                 }
                 .padding(.vertical)
